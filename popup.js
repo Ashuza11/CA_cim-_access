775ee2dd1ab0a74c1d60a7923eabda5719f4803f
ashuza11 <ashuza1411@gmail.com>
2021-11-23 07:19:23 +0200

    first commit

---
+++ popup.js
@@ -0,0 +1,37 @@
+const openModalButtons = document.querySelectorAll('[data-form-target]')
+const closeModalButtons = document.querySelectorAll('[data-colse-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const form = document.querySelector(button.dataset.formTarget)
+        openModal(form)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const form = button.closest('.form')
+        closeModal(form)
+    })
+})
+
+
+overlay.addEventListener('click', () => {
+        const modals = document.querySelectorAll('.form.active')
+        modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+function openModal(form) {
+    if (form == null) return
+    form.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(form) {
+    if (form == null) return
+    form.classList.remove('active')
+    overlay.classList.remove('active')
+}
